34eee0078b5cd328b465f672dc9983375719b76a
yeurslovingly <76031779+yeurslovingly@users.noreply.github.com>
2021-11-14 17:16:35 +0800

    JKF's update
    Added javadoc to the class

--- TableGenerator.java
+++ TableGenerator.java
@@ -2,16 +2,37 @@ import java.util.*;
 /**
  * 
  * @author A
- * THis entire class helps to faciliate printing neat and tide tables for our contents
+ * This entire class helps to faciliate printing neat and tidy tables for our contents
  */
 public class TableGenerator {
-
+	/**
+	 * int PADDING_SIZE is the space between its content and its border
+	 */
     private int PADDING_SIZE = 2;
+    /**
+     * String NEW_LINE prints a new line
+     */
     private String NEW_LINE = "\n";
+    /**
+     *  String TABLE_JOINT_SYMBOL prints the symbol joining the columns
+     */
     private String TABLE_JOINT_SYMBOL = "+";
+    /**
+     * String TABLE_V_SPLIT_SYMBOL prints the symbol splitting the columns
+     */
     private String TABLE_V_SPLIT_SYMBOL = "|";
+    /**
+     * String TABLE_H_SPLIT_SYMBOL prints the symbol splitting the rows between the header and the data
+     */
     private String TABLE_H_SPLIT_SYMBOL = "-";
-
+    
+    /**
+     * This method generates the Table which holds the data
+     * @param headersList - gets the content of the headers
+     * @param rowsList - gets the content of the row's details
+     * @param overRiddenHeaderHeight - overriding the HeaderHeight to not exceed more than 1 line
+     * @return returns String of mutable sequence of characters
+     */
     public String generateTable(List<String> headersList, List<List<String>> rowsList,int... overRiddenHeaderHeight)
     {
         StringBuilder stringBuilder = new StringBuilder();
@@ -54,14 +75,23 @@ public class TableGenerator {
 
         return stringBuilder.toString();
     }
-
+    /**
+     * This method deals with filling the space between the data
+     * @param stringBuilder - gets the mutable sequence of characters
+     * @param length - gets the length of the padding between the Strings of the data
+     */
     private void fillSpace(StringBuilder stringBuilder, int length)
     {
         for (int i = 0; i < length; i++) {
             stringBuilder.append(" ");
         }
     }
-
+    /**
+     * This method deals with the creation of individual rows of the table
+     * @param stringBuilder - gets the mutable sequence of characters 
+     * @param headersListSize - gets the size of the Headers List
+     * @param columnMaxWidthMapping - gets the maximum width of the column 
+     */
     private void createRowLine(StringBuilder stringBuilder,int headersListSize, Map<Integer,Integer> columnMaxWidthMapping)
     {
         for (int i = 0; i < headersListSize; i++) {
@@ -77,7 +107,12 @@ public class TableGenerator {
         }
     }
 
-
+    /**
+     * This method deals with the maximum width of the table
+     * @param headersList - gets the ArrayList of headers
+     * @param rowsList - gets the content of rowsList
+     * @return the interface of objects that maps keys to values
+     */
     private Map<Integer,Integer> getMaximumWidhtofTable(List<String> headersList, List<List<String>> rowsList)
     {
         Map<Integer,Integer> columnMaxWidthMapping = new HashMap<>();
@@ -117,7 +152,14 @@ public class TableGenerator {
 
         return columnMaxWidthMapping;
     }
-
+    /**
+     * This method deals with the optimal cell padding for the table
+     * @param cellIndex - gets the index of the cell
+     * @param datalength - the length of the individual data
+     * @param columnMaxWidthMapping - the maximum width of the column
+     * @param cellPaddingSize - the size of the individual Padding Cell
+     * @return the int of optimum cell padding
+     */
     private int getOptimumCellPadding(int cellIndex,int datalength,Map<Integer,Integer> columnMaxWidthMapping,int cellPaddingSize)
     {
         if(datalength % 2 != 0)
@@ -132,7 +174,13 @@ public class TableGenerator {
 
         return cellPaddingSize;
     }
-
+    /**
+     * The method deals with the filling of the cell with data
+     * @param stringBuilder - gets the stringBuilder String
+     * @param cell - gets the String of individual data
+     * @param cellIndex - the index of the cell
+     * @param columnMaxWidthMapping - gets the max width for mapping
+     */
     private void fillCell(StringBuilder stringBuilder,String cell,int cellIndex,Map<Integer,Integer> columnMaxWidthMapping)
     {
 
@@ -155,7 +203,11 @@ public class TableGenerator {
         stringBuilder.append(TABLE_V_SPLIT_SYMBOL); 
 
     }
-
+    /**
+     * This method deals with the printing of the table
+     * @param m - gets the ArrayList of MenuItems
+     * @param a - gets the number from which the MenuItems are printed in the Menu
+     */
     public static void printtable(ArrayList<MenuItems> m,int a) {
         TableGenerator tableGenerator = new TableGenerator();
 
@@ -203,7 +255,11 @@ public class TableGenerator {
 
         System.out.println(tableGenerator.generateTable(headersList, rowsList));
     }
-    
+    /**
+     * This method deals with the printing of individual MenuItems of package contents
+     * @param m - gets the ArrayList of MenuItems
+     * @param name - gets the name of the Package whose contents are printed
+     */
     public static void printpackagecontents(ArrayList<MenuItems> m,String name) {
         TableGenerator tableGenerator = new TableGenerator();
         
@@ -250,7 +306,11 @@ public class TableGenerator {
 
         System.out.println(tableGenerator.generateTable(headersList, rowsList));
     }
-    
+    /**
+     * This method deals with the printing of the invoice
+     * @param m - gets the ArrayList of MenuItems
+     * @param price - gets the corresponding price of the said items (total price of the items)
+     */
     public static void printinvoice(ArrayList<MenuItems> m,ArrayList<Double> price) {
         TableGenerator tableGenerator = new TableGenerator();
         System.out.println("----------------------------------------");
@@ -267,6 +327,7 @@ public class TableGenerator {
         MenuItems t; 
         for (int i = 0; i < m.size() ; i++) {
         	List<String> row = new ArrayList<>(); 
+        	//t= MainMenu.searchByName(m.get(i).getName());
         	t= MainMenuMgr.Menu.searchByName(m.get(i).getName());
         	double one=t.getPrice();
         	int q=(int) (price.get(i)/one);
